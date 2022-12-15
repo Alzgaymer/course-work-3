36c72f3348bfb1c12fad8879ea1eb3256eec6ddd
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2022-12-16 00:02:37 +0200

    Init proj

---
+++ course-work-3/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace course_work_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pMemory = new System.Windows.Forms.ProgressBar();
+            this.pCPU = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(216, 218);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "CPU";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Memory";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(367, 311);
+            this.chart1.TabIndex = 4;
+            // 
+            // pMemory
+            // 
+            this.pMemory.Location = new System.Drawing.Point(206, 160);
+            this.pMemory.Name = "pMemory";
+            this.pMemory.Size = new System.Drawing.Size(427, 31);
+            this.pMemory.TabIndex = 3;
+            // 
+            // pCPU
+            // 
+            this.pCPU.Location = new System.Drawing.Point(206, 103);
+            this.pCPU.Name = "pCPU";
+            this.pCPU.Size = new System.Drawing.Size(427, 31);
+            this.pCPU.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPU";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(89, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Memory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 541);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pMemory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pCPU);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ProgressBar pMemory;
+        private System.Windows.Forms.ProgressBar pCPU;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ course-work-3/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace course_work_3
+{
+    public partial class Form1 : Form
+    {
+        static System.Timers.Timer _timer;
+        public Form1()
+        {
+            InitializeComponent();
+            _timer = new System.Timers.Timer();
+            _timer.Interval = 1000;
+            _timer.Elapsed += OnTimeEvent;
+            CheckForIllegalCrossThreadCalls = false;
+            _timer.Start();
+        }
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            int cpu = GetCpuValue();
+            int memory = GetMemValue();
+            pCPU.Value = cpu;
+            pMemory.Value = memory;
+            
+            ProgressBarInvoke(pCPU, cpu);
+            ProgressBarInvoke(pMemory, memory);
+
+            ChartInvoke(chart1, "CPU", cpu);
+            ChartInvoke(chart1, "Memory", memory);
+            
+        }
+        void ChartInvoke(Chart c, string category, int value)
+        {
+            if (chart1.InvokeRequired) chart1.Invoke(new Action(() => c.Series[category].Points.AddY(value)));
+            else c.Series[category].Points.AddY(value);
+        }
+        void ProgressBarInvoke(ProgressBar p, int value)
+        {
+            if (p.InvokeRequired) p.Invoke(new Action(() => p.Value = value));
+            else p.Value = value;
+        }
+        private static int GetCpuValue()
+        {
+            var cpu = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            cpu.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            return (int)cpu.NextValue(); 
+        }
+        private static int GetMemValue()
+        {
+            var memory = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+            memory.NextValue();
+            
+            return (int)memory.NextValue(); 
+        }
+    }
+}
